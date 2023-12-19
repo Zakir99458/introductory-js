23ed87e2f47a3c67b169a513b4375a2e8e9a293c
Mohammad Zakir Hossain <engineer.zakir.hossain@gmail.com>
2023-12-19 23:36:00 +0200

    object-pro-sh

---
+++ object.js
@@ -0,0 +1,21 @@
+var student1 = {
+    id: 1, 
+    name: "Mohammad Zakir",
+    marks: 81,
+    phone: "+358 41 72"
+}
+var mobile = {
+    color: "black",
+    size: 5,
+    price: 15000,
+    ramCapacity: "15Gb"
+}
+var book = {
+    title: "AI",
+    price: "150",
+    author: "Osman",
+    pages: 246,
+    publisher: "Penguine"
+}
+
+console.log(book.publisher);
