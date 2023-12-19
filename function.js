986cff60d45f781319f4533414b8785beedf2dc9
Mohammad Zakir Hossain <engineer.zakir.hossain@gmail.com>
2023-12-19 23:07:39 +0200

    function-parameter

--- function.js
+++ function.js
@@ -1,6 +1,19 @@
 function startFan(){
     console.log("Press the switch on")
 }
-
 // Call the function
 startFan();
+
+// Function with parameter
+
+function bringSomething(money){
+    if(money>100)
+        result = "You can buy banana";
+    else
+        result = "Money is not enough";
+
+    return result;
+}
+var money = 102;
+outcome = bringSomething(money);
+console.log(outcome);
